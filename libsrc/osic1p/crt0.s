2c48f6e8d3772df30edcd4798433713e0bad9fb4
jeringa <j_eringa@yahoo.co.nz>
2024-10-07 18:52:20 +1300

    Modify the crt.0 so that the loaded cc65 program sets the BASIC hi-mem to just before the program. Also sets the X=USR(X) vector properly

--- libsrc/osic1p/crt0.s
+++ libsrc/osic1p/crt0.s
@@ -11,8 +11,9 @@
 .import   __MAIN_START__, __MAIN_SIZE__ ; Linker generated
 .import   __STACKSIZE__
 
-.import    zerobss, initlib, donelib
-
+.import   zerobss, initlib, donelib
+.import   __LOADADDR__
+        
 .include  "zeropage.inc"
 .include  "extzp.inc"
 .include  "osic1p.inc"
@@ -31,7 +32,22 @@ _init:    ldx     #$FF          ; Initialize stack pointer to $01FF
 
 ; ---------------------------------------------------------------------------
 ; Set cc65 argument stack pointer
+_init2:
+          lda     #<(_init2)
+          sta     zp_bas_usrJumpAddr            ; Allows us to X=USR(X) from basic to this program
+          lda     #>(_init2)
+          sta     zp_bas_usrJumpAddr+1
 
+          lda     #<(__MAIN_START__ - 1)    ; ADDR just before us in memory
+;          sta     zp_bas_emptyRAM        ; Set the TOP of basic RAM to be before our program
+          sta     zp_bas_strings          ; Strings *START* at the top of free memory and work down
+          sta     zp_bas_memTop
+
+          lda     #>(__MAIN_START__ - 1)    ;
+;          sta     zp_bas_emptyRAM+1         ;
+          sta     zp_bas_strings+1          ;
+          sta     zp_bas_memTop+1
+;
           lda     #<(__MAIN_START__ + __MAIN_SIZE__  + __STACKSIZE__)
           ldx     #>(__MAIN_START__ + __MAIN_SIZE__  + __STACKSIZE__)
           sta     sp
